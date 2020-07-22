e591fca07ab9c5d451c646cb50c3f40e153424b8
Emil Elgaard <shivan@shivan.dk>
2020-07-21 21:59:16 -0400

    add primitive unit tests

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -1,5 +1,14 @@
-from crossref_xml_parser import validate_xml
+from crossref_xml_parser import validate_xml,generate_crossref_xml
 
-# ensure that validate xml works correctly
+# test validate xml method
 assert validate_xml('correct_xml.xml') is True
 assert validate_xml('wrong_xml.xml') is False
+
+# test generate_crossref_xml
+    # with author
+authors = [{"first":'Test','last':'author'}]
+generate_crossref_xml('1986', '1', '2', 'Unittest paper title', 12, 24, 123445,contributors=authors,output_file_name='unit_test_xml.xml')
+assert validate_xml('unit_test_xml.xml') is True
+    # without authors
+generate_crossref_xml('1986', '1', '2', 'Unittest paper title', 12, 24, 123445,output_file_name='unit_test_xml.xml')
+assert validate_xml('unit_test_xml.xml') is True
